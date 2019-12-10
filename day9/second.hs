abfe1596c2ddac98c03ba0113f2fbf250a7c9dd3
Marko Puza <markopuza@gmail.com>
2019-12-10 11:38:28 +0100

    cleaning up ninth day

--- day9/second.hs
+++ day9/second.hs
@@ -10,8 +10,10 @@ type Opcode = (Int, [Int])
 type Settings = [Int]
 data Computer = Computer {ptr :: Int, tape :: Tape, ins :: [Int], outs :: [Int], relBase :: Int}
 
+memSize = 10^6
+
 getTape :: String -> Tape
-getTape str = Seq.fromList $ (map read $ splitOn "," str) ++ replicate 1000000 0
+getTape str = Seq.fromList $ (map read $ splitOn "," str) ++ replicate memSize 0
 
 (!) :: Tape -> Int -> Int
 (!) t i
@@ -32,17 +34,15 @@ parseOp n = (n `mod` 100, [getDigit 2, getDigit 3, getDigit 4])
 
 execute :: Computer -> Computer
 execute comp@Computer{..} = case op of
- -- | trace ("ptr = " ++ show ptr ++ "|" ++ show op ++ " " ++ show (arg 0) ++ " " ++ show digs ++ "   " ++ show ins ++ " " ++ show (arg 1) ++ " " ++ show (val 1)) False = undefined
- -- | otherwise case op of
-      1  -> execute comp{ptr=ptr + 4, tape=set (address 3) (val 1 + val 2) tape, ins=ins,      outs=outs,              relBase=relBase }
-      2  -> execute comp{ptr=ptr + 4, tape=set (address 3) (val 1 * val 2) tape, ins=ins,      outs=outs,              relBase=relBase }
-      3  -> execute comp{ptr=ptr + 2, tape=set (address 1) (head ins) tape,   ins=tail ins, outs=outs,                 relBase=relBase }
-      4  -> execute comp{ptr=ptr + 2, tape=tape,                             ins=ins,      outs=(val 1:outs),      relBase=relBase }
-      5  -> execute comp{ptr=if val 1 > 0 then val 2 else ptr+3,  tape=tape, ins=ins,      outs=outs,              relBase=relBase }
-      6  -> execute comp{ptr=if val 1 == 0 then val 2 else ptr+3, tape=tape, ins=ins,      outs=outs,              relBase=relBase }
-      7  -> execute comp{ptr=ptr + 4, tape=set (address 3) (if val 1 < val 2 then 1 else 0) tape, ins=ins, outs=outs,  relBase=relBase }
-      8  -> execute comp{ptr=ptr + 4, tape=set (address 3) (if val 1 == val 2 then 1 else 0) tape, ins=ins, outs=outs, relBase=relBase }
-      9  -> execute comp{ptr=ptr + 2, tape=tape, ins=ins, outs=outs,                                       relBase=relBase + val 1 }
+      1  -> execute comp{ptr=ptr + 4, tape=set (address 3) (val 1 + val 2) tape,                   ins=ins, outs=outs,         relBase=relBase }
+      2  -> execute comp{ptr=ptr + 4, tape=set (address 3) (val 1 * val 2) tape,                   ins=ins, outs=outs,         relBase=relBase }
+      3  -> execute comp{ptr=ptr + 2, tape=set (address 1) (head ins) tape,                   ins=tail ins, outs=outs,         relBase=relBase }
+      4  -> execute comp{ptr=ptr + 2, tape=tape,                                                   ins=ins, outs=(val 1:outs), relBase=relBase }
+      5  -> execute comp{ptr=if val 1 > 0 then val 2 else ptr+3,  tape=tape,                       ins=ins, outs=outs,         relBase=relBase }
+      6  -> execute comp{ptr=if val 1 == 0 then val 2 else ptr+3, tape=tape,                       ins=ins, outs=outs,         relBase=relBase }
+      7  -> execute comp{ptr=ptr + 4, tape=set (address 3) (if val 1 < val 2 then 1 else 0)  tape, ins=ins, outs=outs,         relBase=relBase }
+      8  -> execute comp{ptr=ptr + 4, tape=set (address 3) (if val 1 == val 2 then 1 else 0) tape, ins=ins, outs=outs,         relBase=relBase }
+      9  -> execute comp{ptr=ptr + 2, tape=tape,                                                   ins=ins, outs=outs, relBase=relBase + val 1 }
       99 -> comp --halt
     where
       address i = if (digs !! (i-1)) == 2 then (tape ! (ptr + i)) + relBase else tape ! (ptr + i)
